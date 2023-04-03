ff34499d25c79751b74e00a9764d58281afd3dff
ysato <56924527+ysato@users.noreply.github.com>
2023-04-03 15:16:27 +0900

    feat: first commit by chatgpt

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,70 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useRef, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState('');
+  const [outputValue, setOutputValue] = useState('');
+
+  const outputTextArea = useRef<HTMLTextAreaElement>(null);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const formatVmstatLog = (log: string) => {
+    const lines = log.split('\n');
+    const parsedLines = lines.map((line) => line.trim().split(/\s+/));
+
+    const maxCharCounts = parsedLines.reduce((maxCounts: number[], line) => {
+      line.forEach((item, index) => {
+        // eslint-disable-next-line no-param-reassign
+        maxCounts[index] = Math.max(item.length, maxCounts[index] || 0);
+      });
+      return maxCounts;
+    }, []);
+
+    const formattedLines = parsedLines.map((line) =>
+      line.map((item, index) => item.padStart(maxCharCounts[index], ' ')).join(' '),
+    );
+
+    return formattedLines.join('\n');
+  };
+
+  const handleButtonClick = () => {
+    const formattedLog = formatVmstatLog(inputValue);
+    setOutputValue(formattedLog);
+    setTimeout(() => {
+      if (outputTextArea.current) {
+        outputTextArea.current.select();
+      }
+    }, 0);
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="app">
+      <h2>LOG FORMATTER</h2>
+      <div>
+        <textarea
+          id="inputTextArea"
+          value={inputValue}
+          onChange={handleInputChange}
+          rows={10}
+          cols={50}
+        />
+        <button type="button" onClick={handleButtonClick}>
+          Format / Beautify
+        </button>
+        <textarea
+          id="outputTextArea"
+          ref={outputTextArea}
+          value={outputValue}
+          readOnly
+          rows={10}
+          cols={50}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
